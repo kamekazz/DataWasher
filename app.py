1d9f2ab998345333a10cc6e842bceb95c045f9f4
kamekazz <taveras78@gmail.com>
2025-06-28 14:51:59 -0400

    learn route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/greet/<name>")
+def greet(name):
+    return f"<p>Hello, {name}!</p>"
+
+@app.route('/handle_url_params')
+def handle_url_params():
+    greeting = request.args.get('greeting', 'default_value1')
+    name = request.args.get('name', 'default_value2')
+    return f"<h2>greeting: {greeting}, name: {name}</h2>"
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0', port=5000)
