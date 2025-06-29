b0fb4f86dc393cf3601d12cb8321ddafbdda8352
kamekazz <taveras78@gmail.com>
2025-06-29 13:03:10 -0400

    Load environment variables in create_app function and add Gunicorn configuration file

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import os
 
 
 def create_app():
+    load_dotenv()
     app = Flask(__name__)
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///datawasher.db"
